e186ac4eaa47bd3b913bd251d447b70df4ec9235
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-27 14:12:40 -0500

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.make = "Nissan";
+            myCar.model = "Altima";
+            myCar.year = 2012;
+
+            Console.WriteLine($"{myCar.year} {myCar.make} {myCar.model}");
+        }
+    }
+}
